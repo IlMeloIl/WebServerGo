3072cc4c500e172174135c13610d369ef308d337
IlMeloIl <jgvmelo123@gmail.com>
2025-03-10 15:56:38 -0300

    add /healthz && change root handler to /app/

--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
-	serveMux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	if err := http.ListenAndServe(":8080", serveMux); err != nil {
 		fmt.Println(err)
 	}
