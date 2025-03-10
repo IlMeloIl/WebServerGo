94bc71fd93d12dbf58253dd5a502246e0ee45947
IlMeloIl <jgvmelo123@gmail.com>
2025-03-10 12:22:35 -0300

    add handler for assets/logo.png

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
 	if err := http.ListenAndServe(":8080", serveMux); err != nil {
 		fmt.Println(err)
 	}
