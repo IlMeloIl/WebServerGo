6ab035acd0a214c06da046cf2a1e2c2e21e765d5
IlMeloIl <jgvmelo123@gmail.com>
2025-03-11 16:17:41 -0300

    Storage

--- apiCfgMethods.go
+++ apiCfgMethods.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"GoServer/internal/database"
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -13,7 +18,17 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) middlewareMetricsReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.Platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
+
+	if err := cfg.DB.DeleteAllUsers(r.Context()); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error deleting all users", err)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
 }
 
@@ -28,4 +43,95 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 		</body>
 	</html>
 	`, cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+	userUnmarshallInto := User{}
+	if err := json.NewDecoder(r.Body).Decode(&userUnmarshallInto); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error unmarshalling User", err)
+	}
+	user, err := cfg.DB.CreateUser(r.Context(), userUnmarshallInto.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating User", err)
+	}
+	data, err := json.Marshal(User{ID: user.ID, CreatedAt: user.CreatedAt, UpdatedAt: user.UpdatedAt, Email: user.Email})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error marshalling JSON", err)
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(data)
+}
+
+func (cfg *apiConfig) createChirp(w http.ResponseWriter, r *http.Request) {
+	chirpUnmarshallInto := Chirp{}
+	if err := json.NewDecoder(r.Body).Decode(&chirpUnmarshallInto); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error unmarshalling Chirp", err)
+	}
+	const maxChirpLen = 140
+	if len(chirpUnmarshallInto.Body) > maxChirpLen {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+	chirp, err := cfg.DB.CreateChirp(r.Context(), database.CreateChirpParams{Body: chirpUnmarshallInto.Body, UserID: chirpUnmarshallInto.UserId})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating chirp", err)
+	}
+	data, err := json.Marshal(Chirp{ID: chirp.ID, CreatedAt: chirp.CreatedAt, UpdatedAt: chirp.UpdatedAt, Body: chirp.Body, UserId: chirp.UserID})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error marshalling Chirp", err)
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(data)
+}
+
+func (cfg *apiConfig) getChirps(w http.ResponseWriter, r *http.Request) {
+	chirps, err := cfg.DB.GetChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get all chirps", err)
+	}
+	chirpsResponse := make([]Chirp, len(chirps))
+	for i, chirp := range chirps {
+		chirpsResponse[i] = Chirp{
+			ID:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserId:    chirp.UserID,
+		}
+	}
+	respondWithJSON(w, http.StatusOK, chirpsResponse)
+}
+
+func (cfg *apiConfig) getChirpByID(w http.ResponseWriter, r *http.Request) {
+
+	chirpIDStr := r.PathValue("chirpID")
+
+	chirpID, err := uuid.Parse(chirpIDStr)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Invalid chirp ID format", err)
+		return
+	}
+
+	chirp, err := cfg.DB.GetChirpByID(r.Context(), chirpID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			respondWithError(w, http.StatusNotFound, "Chirp not found", nil)
+			return
+		}
+		respondWithError(w, http.StatusInternalServerError, "Error retrieving chirp", err)
+		return
+	}
+
+	chirpResponse := Chirp{
+		ID:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserId:    chirp.UserID,
+	}
+
+	respondWithJSON(w, http.StatusOK, chirpResponse)
+
 }
